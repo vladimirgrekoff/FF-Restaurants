12f90142a5a2aeb8f42a7cb7f36b12f8d638698e
Viktoria Masa <viktoriya.masa@gmail.com>
2023-07-05 07:27:37 +0300

    Issue #22. Created the Category Repository, the GroupDish Repository

---
+++ src/main/java/ru/findFood/rest/entities/GroupDish.java
@@ -0,0 +1,38 @@
+package ru.findFood.rest.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "group_dishes")
+@NoArgsConstructor
+@Data
+public class GroupDish {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    //@OneToMany(mappedBy = "groupDish")
+    //private List<Dish> dishes;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
