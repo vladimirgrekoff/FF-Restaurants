e61b0a80aa8f71e81d9e85a2804c91e8b424abdc
Владимир Греков <100429195+vladimirgrekoff@users.noreply.github.com>
2023-07-06 06:57:06 +0700

    Delete src directory

--- src/main/java/ru/findFood/rest/repositories/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.findFood.rest.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.findFood.rest.entities.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
