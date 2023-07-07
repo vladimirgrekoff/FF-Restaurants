e06c017971272aff7908b18ca8cf5b8468a4721b
Viktoria Masa <viktoriya.masa@gmail.com>
2023-07-07 03:02:44 +0300

    Issue #39. Created Restaurant DTO, Restaurant Info DTO

--- src/main/java/ru/findFood/rest/dtos/GroupDishDto.java
+++ src/main/java/ru/findFood/rest/dtos/GroupDishDto.java
@@ -2,6 +2,9 @@ package ru.findFood.rest.dtos;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import ru.findFood.rest.dtos.DishDto;
@@ -11,6 +14,9 @@ import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Schema(description = "Модель группы блюд")
 public class GroupDishDto {
     @Schema(description = "ID группы блюд",  requiredMode = Schema.RequiredMode.AUTO, example = "4")
@@ -19,32 +25,4 @@ public class GroupDishDto {
     @Schema(description = "Имя группы блюд",  requiredMode = Schema.RequiredMode.AUTO, example = "Супы")
     private String title;
 
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-
-
-    public GroupDishDto() {
-    }
-
-
-    public GroupDishDto(Long id, String title) {
-        this.id = id;
-        this.title = title;
-    }
 }
