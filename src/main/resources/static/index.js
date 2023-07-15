6caa1fdc551fabf8084fb2e5a9d55ce6e5825bee
Владимир Греков <100429195+vladimirgrekoff@users.noreply.github.com>
2023-07-15 08:37:25 +0700

    RP70T20 Creating the code that ensures the loading of the list of restaurants (front)

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,43 @@
+(function () {
+    angular
+        .module('findFood', ['ngRoute', 'ngStorage'])
+        .config(config)
+//        .run(run)
+;
+
+    function config($routeProvider, $httpProvider) {
+
+        $routeProvider.when('/welcome', {
+            templateUrl:'welcome/welcomeTemplate.html',
+            controller:'welcomeController',
+            controllerAs:'welcome'
+
+        })
+        .when('/restaurants', {
+            templateUrl:'restaurants/restaurantsTemplate.html',
+            controller:'restaurantsController',
+            controllerAs:'restaurants'
+        })
+        .when('/restaurant', {
+            templateUrl:'restaurant/restaurantTemplate.html',
+            controller:'restaurantController',
+            controllerAs:'restaurant'
+        })
+        .otherwise({
+            redirectTo: '/welcome'
+        });
+    }
+})();
+
+angular.module('findFood').controller('indexController', function ($rootScope, $scope, $http, $location, $localStorage) {
+    $rootScope.isCurrentPageRestaurant = function () {
+        if ($rootScope.currentPage == 'restaurant') {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+
+
+});
