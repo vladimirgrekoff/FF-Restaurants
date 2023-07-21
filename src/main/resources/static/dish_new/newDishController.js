9ef33e405488582d05d5e90058aa91d0cc0840fe
Владимир Греков <100429195+vladimirgrekoff@users.noreply.github.com>
2023-07-22 06:01:10 +0700

    RP50T70 Создание кода обеспечивающего отправку запроса на создание (добавление в БД) нового Блюда (фронт)

--- src/main/resources/static/dish_new/newDishController.js
+++ src/main/resources/static/dish_new/newDishController.js
@@ -31,7 +31,81 @@ angular.module('findFood').controller('newDishController', function ($rootScope,
     $scope.groupList = $localStorage.groupDishList;
     $scope.categoriesList = $localStorage.categoriesDishList;
 
+    $scope.addNewDish = function () {
+        if ($scope.isEmptyNewDishData() == false){
+            $scope.newDish.group_dish_title = $scope.groupDishTitle;
+            $scope.newDish.category_title = $scope.categoryTitle;
+            $http.post(contextPath, $scope.newDish)
+                .then(function (response) {
+                    $scope.newDish.title = null;
+                    $scope.newDish.restaurant_title = null;
+                    $scope.newDish.description = null;
+                    $scope.newDish.price = null;
+                    $scope.newDish.calories = null;
+                    $scope.newDish.proteins = null;
+                    $scope.newDish.fats = null;
+                    $scope.newDish.carbohydrates = null;
+                    $scope.newDish.healthy = null;
+                    $scope.newDish.approved = null;
+                    $scope.groupDishTitle = $scope.selected;
+                    $scope.categoryTitle = $scope.selected;
+                    alert("Новое блюдо добавлено");
+                });
+        }
+    };
 
+
+    $scope.isEmptyNewDishData = function(){
+        if($scope.newDish == undefined || $scope.newDish == null){
+            alert("При добавлении нового блюда все поля должны быть заполнены!");
+            return true;
+        }
+        if($scope.newDish.title == undefined || $scope.newDish.title == null || $scope.newDish.title == ''){
+            alert("Поле 'Название' должно быть заполнено!");
+            return true;
+        }
+        if($scope.newDish.description == undefined || $scope.newDish.description == null || $scope.newDish.description == ''){
+            alert("Поле 'Описание' должно быть заполнено!");
+            return true;
+        }
+        if($scope.groupDishTitle == undefined || $scope.groupDishTitle == ''){
+            alert("'Группа блюд' должна иметь одно из значений в списке 'Выбор группы'!");
+            return true;
+        }
+        if($scope.newDish.calories == undefined || $scope.newDish.calories == null || $scope.newDish.calories == ''){
+            if(angular.isNumber($scope.newDish.calories) == false){
+                alert("Поле 'Калории' должно быть заполнено!");
+                return true;
+            }
+        }
+        if($scope.newDish.fats == undefined || $scope.newDish.fats == null || $scope.newDish.fats == ''){
+            if(angular.isNumber($scope.newDish.fats) == false){
+                alert("Поле 'Жиры' должно быть заполнено!");
+                return true;
+            }
+        }
+        if($scope.newDish.proteins == undefined || $scope.newDish.proteins == null || $scope.newDish.proteins == ''){
+            if(angular.isNumber($scope.newDish.proteins) == false){
+                alert("Поле 'Белки' должно быть заполнено!");
+                return true;
+            }
+        }
+        if($scope.newDish.carbohydrates == undefined || $scope.newDish.carbohydrates == null || $scope.newDish.carbohydrates == ''){
+            if(angular.isNumber($scope.newDish.carbohydrates) == false){
+                alert("Поле 'Углеводы' должно быть заполнено!");
+                return true;
+            }
+        }
+        if($scope.newDish.price == undefined || $scope.newDish.price == null || $scope.newDish.price == 0 || $scope.newDish.price == ''){
+            alert("Поле 'Стоимость' должно быть заполнено!");
+            return true;
+        }
+        if($scope.categoryTitle == undefined || $scope.categoryTitle == ''){
+            alert("'Категория блюд' должна иметь одно из значений в списке 'Выбор категории'!");
+            return true;
+        }
+        return false;
+    };
 
 
     //переходы
