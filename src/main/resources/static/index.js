e82d8e78aa2a317fd0c4b5026d72928d76d66005
Владимир Греков <100429195+vladimirgrekoff@users.noreply.github.com>
2023-07-22 05:59:00 +0700

    RP50T60 Создание кода обеспечивающего загрузку формы Добавить блюдо (фронт)

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -38,6 +38,16 @@
             controller:'dishController',
             controllerAs:'dish'
         })
+        .when('/dish_edit', {
+            templateUrl:'dish_edit/dishEditTemplate.html',
+            controller:'dishEditController',
+            controllerAs:'dish_edit'
+        })
+        .when('/dish_new', {
+            templateUrl:'dish_new/newDishTemplate.html',
+            controller:'newDishController',
+            controllerAs:'dish_new'
+        })
         .otherwise({
             redirectTo: '/welcome'
         });
@@ -64,6 +74,22 @@ angular.module('findFood').controller('indexController', function ($rootScope, $
 
     $rootScope.isCurrentPageDish = function () {
         if ($rootScope.currentPage == 'dish') {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    $rootScope.isCurrentPageEditDish = function () {
+        if ($rootScope.currentPage == 'dish_edit') {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    $rootScope.isCurrentPageAddNewDish = function () {
+        if ($rootScope.currentPage == 'dish_new') {
             return true;
         } else {
             return false;
