1d0eb09c97b4e2ab78ed062fa2a0478f67965d60
Владимир Греков <100429195+vladimirgrekoff@users.noreply.github.com>
2023-07-22 07:34:14 +0700

    Revert "RP30T90 Создание кода загрузки информации о ресторане на страницу-визитку (фронт)"
    This reverts commit fd39a0087d8be68bfaa139a24ecaa64743089d69.

--- src/main/resources/static/restaurant/restaurantController.js
+++ src/main/resources/static/restaurant/restaurantController.js
@@ -5,7 +5,6 @@ angular.module('findFood').controller('restaurantController', function ($rootSco
     //включение дополнительных пунктов меню для страницы
     $rootScope.currentPage = 'restaurant';
 
-
     var restaurant;
 
     restaurant = $localStorage.currentRestaurant;
@@ -15,6 +14,9 @@ angular.module('findFood').controller('restaurantController', function ($rootSco
     };
 
 
+    $scope.showRestaurantDishes = function () {
+        return $localStorage.currentRestaurant.dishesList;
+    };
 
     $scope.loadRestaurantInfo = function(restaurant){
         var id;
@@ -25,5 +27,40 @@ angular.module('findFood').controller('restaurantController', function ($rootSco
             });
     };
 
+    //обработка выбора пунктов меню
+    $scope.showDishes = true;
+    $rootScope.showDishesList = function () {
+        if(!$scope.showInfo){
+            //Если откыта информация, закрыть
+            $rootScope.showRestaurantInfo();
+        }
+        if($scope.showDishes){
+            $scope.showDishes = false;
+            return $scope.showDishes;
+        } else {
+            $scope.showDishes = true;
+            return $scope.showDishes;
+        }
+    };
+
+    $scope.showInfo = true;
+    $rootScope.showRestaurantInfo = function () {
+        if(!$scope.showDishes){
+            //Если откыт список блюд, закрыть
+            $rootScope.showDishesList();
+        }
+        if($scope.showInfo){
+            $scope.showInfo = false;
+            return $scope.showInfo;
+        } else {
+            $scope.showInfo = true;
+            return $scope.showInfo;
+        }
+    };
+
+
+    $scope.showRestaurantPage = function () {
+        $location.path('restaurant');
+    };
     $scope.loadRestaurantInfo();
 });
