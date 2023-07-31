f76cc29b692f22bce5ea6f2192573b20fd6f83d7
Владимир Греков <100429195+vladimirgrekoff@users.noreply.github.com>
2023-07-31 08:24:46 +0700

    RP60T120 Создание сущности элементов собранных запросов диетологу

---
+++ src/main/java/ru/findFood/rest/utils/MailBoxItem.java
@@ -0,0 +1,24 @@
+package ru.findFood.rest.utils;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MailBoxItem {
+    private Long dishId;
+    private String dishTitle;
+    private String dishDescription;
+    private String dishGroupDishTitle;
+    private Integer dishCalories;
+    private Integer dishProteins;
+    private Integer dishFats;
+    private Integer dishCarbohydrates;
+    private Boolean dishHealthy;
+    private Boolean dishApproved;
+}
+
