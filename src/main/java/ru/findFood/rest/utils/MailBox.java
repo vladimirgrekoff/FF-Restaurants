9bd7724df2641583eb1e42fa7898e712cd40e4aa
Владимир Греков <100429195+vladimirgrekoff@users.noreply.github.com>
2023-07-31 08:23:14 +0700

    RP60T110 Создание сущности собранных запросов диетологу

---
+++ src/main/java/ru/findFood/rest/utils/MailBox.java
@@ -0,0 +1,67 @@
+package ru.findFood.rest.utils;
+
+import lombok.Data;
+import ru.findFood.rest.entities.Dish;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Data
+public class MailBox {
+    private List<MailBoxItem> items;
+
+    public MailBox(){
+        this.items = new ArrayList<>();
+    }
+
+    public List<MailBoxItem> getItems() {
+        return items;
+    }
+
+    public void add(Dish d) {
+        if(items != null) {
+            for (MailBoxItem item : items) {
+                if (item.getDishId().equals(d.getId())) {
+                    return;
+                }
+            }
+        }
+        MailBoxItem mailBoxItem = new MailBoxItem(
+                d.getId(),
+                d.getTitle(),
+                d.getDescription(),
+                d.getGroupDish().getTitle(),
+                d.getCalories(),
+                d.getProteins(),
+                d.getFats(),
+                d.getCarbohydrates(),
+                d.getHealthy(),
+                d.getApproved()
+        );
+        items.add(mailBoxItem);
+    }
+    public void remove(Long id) {
+        for (MailBoxItem item : items) {
+            if (item.getDishId().equals(id)) {
+                items.remove(item);
+                return;
+            }
+        }
+    }
+
+    public boolean isPresentInMailBox(Long id) {
+        boolean result = false;
+        for (MailBoxItem item : items) {
+            if(item.getDishId() == id) {
+                result = true;
+            }
+        }
+        return result;
+    }
+    public void clear() {
+        items.clear();
+    }
+
+
+}
