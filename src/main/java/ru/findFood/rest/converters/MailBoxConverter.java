67f94e2c2ce7a001272a17a3723974e12acda3d0
Владимир Греков <100429195+vladimirgrekoff@users.noreply.github.com>
2023-07-31 08:30:48 +0700

    RP60T150 Создание конвертера собранных запросов диетологу

---
+++ src/main/java/ru/findFood/rest/converters/MailBoxConverter.java
@@ -0,0 +1,18 @@
+package ru.findFood.rest.converters;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.findFood.rest.dtos.MailBoxDto;
+import ru.findFood.rest.utils.MailBox;
+
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class MailBoxConverter {
+    private final MailBoxItemConverter mailBoxItemConverter;
+
+    public MailBoxDto entityToDto(MailBox mailBox) {
+        return new MailBoxDto(mailBox.getItems().stream().map(mailBoxItemConverter::entityToDto).collect(Collectors.toList()));
+    }
+}
