9d0019a71f80bf1175abca0316f42f54ca1ac6db
Владимир Греков <100429195+vladimirgrekoff@users.noreply.github.com>
2023-07-31 08:39:46 +0700

    RP60T200 Включение ссылок на встроенный Redis в главный файл микросервиса

--- src/main/java/ru/findFood/rest/RestApplication.java
+++ src/main/java/ru/findFood/rest/RestApplication.java
@@ -1,13 +1,32 @@
 package ru.findFood.rest;
 
+import jakarta.annotation.PreDestroy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.findFood.rest.embeddedRedis.RedisServer;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class RestApplication {
+	private static RedisServer redisServer;
 
-	public static void main(String[] args) {
+	@PreDestroy
+	private static void preDestroy(){
+		redisServer.stop();
+	}
+
+	private static void embeddedServerInit() throws IOException {
+		redisServer = new RedisServer(6379);
+		redisServer.start();
+
+	}
+
+	public static void main(String[] args) throws IOException {
+
 		SpringApplication.run(RestApplication.class, args);
+
+		embeddedServerInit();
 	}
 
 }
