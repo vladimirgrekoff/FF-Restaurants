7ee3623c8af99d2232065e1e2e53f2668a6c3bf0
Владимир Греков <100429195+vladimirgrekoff@users.noreply.github.com>
2023-07-31 08:32:00 +0700

    RP60T160 Создание конвертера элементов собранных запросов диетологу

---
+++ src/main/java/ru/findFood/rest/converters/MailBoxItemConverter.java
@@ -0,0 +1,39 @@
+package ru.findFood.rest.converters;
+
+import org.springframework.stereotype.Component;
+import ru.findFood.rest.dtos.MailBoxItemDto;
+import ru.findFood.rest.utils.MailBoxItem;
+
+
+@Component
+public class MailBoxItemConverter {
+
+    public MailBoxItem dtoToEntity(MailBoxItemDto mBIDto) {
+        return new MailBoxItem(
+                mBIDto.getDishId(),
+                mBIDto.getDishTitle(),
+                mBIDto.getDishDescription(),
+                mBIDto.getDishGroupDishTitle(),
+                mBIDto.getDishCalories(),
+                mBIDto.getDishProteins(),
+                mBIDto.getDishFats(),
+                mBIDto.getDishCarbohydrates(),
+                mBIDto.getDishHealthy(),
+                mBIDto.getDishApproved());
+    }
+
+
+    public MailBoxItemDto entityToDto(MailBoxItem mBItem) {
+        return new MailBoxItemDto(
+                mBItem.getDishId(),
+                mBItem.getDishTitle(),
+                mBItem.getDishDescription(),
+                mBItem.getDishGroupDishTitle(),
+                mBItem.getDishCalories(),
+                mBItem.getDishProteins(),
+                mBItem.getDishFats(),
+                mBItem.getDishCarbohydrates(),
+                mBItem.getDishHealthy(),
+                mBItem.getDishApproved());
+    }
+}
